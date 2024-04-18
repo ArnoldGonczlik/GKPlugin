f27e4389a468743234ec90d225299375773f738c
Arnold <arnold.gonczlik@hotmail.com>
2024-04-18 22:32:28 +0200

    Start of GKPlugin

--- src/main/java/com/gkplugin/GKConfig.java
+++ src/main/java/com/gkplugin/GKConfig.java
@@ -1,11 +1,11 @@
-package com.example;
+package com.gkplugin;
 
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
 
 @ConfigGroup("example")
-public interface ExampleConfig extends Config
+public interface GKConfig extends Config
 {
 	@ConfigItem(
 		keyName = "greeting",
--- src/main/java/com/gkplugin/GKPlugin.java
+++ src/main/java/com/gkplugin/GKPlugin.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.gkplugin;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
@@ -16,13 +16,13 @@ import net.runelite.client.plugins.PluginDescriptor;
 @PluginDescriptor(
 	name = "Example"
 )
-public class ExamplePlugin extends Plugin
+public class GKPlugin extends Plugin
 {
 	@Inject
 	private Client client;
 
 	@Inject
-	private ExampleConfig config;
+	private GKConfig config;
 
 	@Override
 	protected void startUp() throws Exception
@@ -46,8 +46,8 @@ public class ExamplePlugin extends Plugin
 	}
 
 	@Provides
-	ExampleConfig provideConfig(ConfigManager configManager)
+	GKConfig provideConfig(ConfigManager configManager)
 	{
-		return configManager.getConfig(ExampleConfig.class);
+		return configManager.getConfig(GKConfig.class);
 	}
 }
